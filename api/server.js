1c7acec6d30b52395369701d1979a6197cd9f7f8
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-04 01:54:27 -0600

    Updated MOTD

--- api/server.js
+++ api/server.js
@@ -10,9 +10,9 @@ server.use(express.json());
 
 server.get("/", async (req, res) => {
   try {
-    const shoutouts = await db("shoutouts");
+    const shouts = await db("shouts");
     const messageOfTheDay = process.env.MOTD || "Hello World!"; // add this line
-    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+    res.status(200).json({ motd: messageOfTheDay, shouts }); // change this line
   } catch (error) {
     console.error("\nERROR", error);
     res.status(500).json({ error: "Cannot retrieve the shoutouts" });
