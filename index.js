dffb9e928bfbfc496700dcbc6cd44591980bb5ab
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-04 01:06:46 -0600

    Added .env file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const server = require('./api/server.js');
-require('dotenv').config()
+// it's recommended to load configuration for .env as early as possible
+require("dotenv").config();
+const server = require("./api/server.js");
 
-const port = process.env.PORT ? process.env.PORT : 4000
+const port = process.env.PORT ? process.env.PORT : 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
