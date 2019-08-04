fd462d196a088fc0b7b6a16061f0d1a7e4b32466
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-04 01:46:14 -0600

    Added Message of the Day get method on '/'

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 require("dotenv").config();
 const server = require("./api/server.js");
 
-const port = process.env.PORT ? process.env.PORT : 4000;
+const port = process.env.PORT || 5000;
+
+server.get("/", async (req, res) => {
+  try {
+    const shoutouts = await db("shoutouts");
+    const messageOfTheDay = process.env.MOTD || "Hello World!"; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+  }
+});
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// const port = process.env.PORT ? process.env.PORT : 4000;
